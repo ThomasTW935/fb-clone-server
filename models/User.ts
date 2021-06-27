248c73ac4c37e7dc1649b2c8f44d95adf89b6431
ThomasTW935 <thomastw935@gmail.com>
2021-06-27 08:41:50 +0800

    Added User Model

---
+++ models/User.ts
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const UserSchema = new Schema({
+  name: {type: String,  required: true},
+  firebase_uid: {type:String, required: true}
+})
+
+const User = mongoose.model('user', UserSchema)
+
+module.exports = User
