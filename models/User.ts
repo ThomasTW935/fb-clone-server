6d71cd3de8c9dc390f6f1f477a0cd8d5db3a0031
ThomasTW935 <thomastw935@gmail.com>
2021-07-01 21:03:51 +0800

    Code Formatted

--- models/User.ts
+++ models/User.ts
@@ -3,8 +3,8 @@ import mongoose from 'mongoose'
 const Schema = mongoose.Schema
 
 const UserSchema = new Schema({
-  name: {type: String,  required: true},
-  firebase_uid: {type:String, required: true}
+  name: { type: String, required: true },
+  firebase_uid: { type: String, required: true },
 })
 
 const User = mongoose.model('user', UserSchema)
--- routes/api/users.ts
+++ routes/api/users.ts
@@ -8,12 +8,25 @@ const User = require('../../models/User')
 // *@desc Get all users
 // ?@acces Public
 
-router.get('/', async (req,res)=>{
-  try{
+router.get('/', async (req, res) => {
+  try {
     const users = await User.find()
     res.json(users)
-  }catch(err){
-    res.status(404).json({'error':err})
+  } catch (err) {
+    res.status(404).json({ error: err })
+  }
+})
+
+// *@route GET api/users/:firebaseuid
+// *@desc Get user with :firebaseuid
+// ?@acces Public
+
+router.get('/:firebase_uid', async (req, res) => {
+  try {
+    const user = await User.findOne({ firebase_uid: req.params.firebase_uid })
+    res.json(user)
+  } catch (err) {
+    res.status(404).json({ error: err })
   }
 })
 
@@ -21,17 +34,16 @@ router.get('/', async (req,res)=>{
 // *@desc Get all users
 // ?@acces Public
 
-router.post('/', async (req,res)=>{
-  console.log("hello")
-  try{
+router.post('/', async (req, res) => {
+  try {
     const newUser = new User({
       name: req.body.name,
-      firebase_uid: req.body.firebase_uid
+      firebase_uid: req.body.firebase_uid,
     })
     const user = await newUser.save()
     res.json(user)
-  }catch(err){
-    res.status(400).json({'error':err})
+  } catch (err) {
+    res.status(400).json({ error: err })
   }
 })
 
