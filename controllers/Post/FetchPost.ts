edc8151f88dbd09bd3ef56bf1f245f717fe18709
ThomasTW935 <thomastw935@gmail.com>
2021-07-09 19:58:27 +0800

    Created Post Fetch Controller

---
+++ controllers/Post/FetchPost.ts
@@ -0,0 +1,20 @@
+const Post = require('../../models/Post')
+
+const fetchAllPost = async (req: any, res: any) => {
+  try {
+    const posts = await Post.find().sort({ createdAt: -1 }).populate('user')
+    res.status(200).json({ post: posts })
+  } catch {
+    res.status(500).json({ error: 'Database Error' })
+  }
+}
+const fetchPostById = async (req: any, res: any) => {
+  try {
+    const post = await Post.findById(req.params.postId).populate('user')
+    res.status(200).json({ post: post })
+  } catch {
+    res.status(500).json({ error: 'Database Error' })
+  }
+}
+
+export { fetchAllPost, fetchPostById }
