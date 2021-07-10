90efbdea4501c842c1320abfa8ffd47940e55248
ThomasTW935 <thomastw935@gmail.com>
2021-07-10 10:13:53 +0800

    Added active in User Schema

--- models/User.ts
+++ models/User.ts
@@ -5,6 +5,7 @@ const Schema = mongoose.Schema
 const UserSchema = new Schema({
   name: { type: String, required: true },
   firebase_uid: { type: String, required: true },
+  active: { type: Boolean, default: false },
 })
 
 const User = mongoose.model('user', UserSchema)
