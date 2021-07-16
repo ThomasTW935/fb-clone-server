32de311ee83f2778ec87c35014f4245e7f3e3f75
ThomasTW935 <thomastw935@gmail.com>
2021-07-17 00:16:48 +0800

    Change to posts from post

--- controllers/Post/FetchPost.ts
+++ controllers/Post/FetchPost.ts
@@ -3,7 +3,7 @@ const Post = require('../../models/Post')
 const fetchAllPost = async (req: any, res: any) => {
   try {
     const posts = await Post.find().sort({ createdAt: -1 }).populate('user')
-    res.status(200).json({ post: posts })
+    res.status(200).json({ posts: posts })
   } catch {
     res.status(500).json({ error: 'Database Error' })
   }
