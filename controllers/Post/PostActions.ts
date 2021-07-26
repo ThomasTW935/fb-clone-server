726a520e0e0e28fd929db2e045f9e86220edfde4
ThomasTW935 <thomastw935@gmail.com>
2021-07-26 16:02:12 +0800

    Added Delete and Update function

--- controllers/Post/PostActions.ts
+++ controllers/Post/PostActions.ts
@@ -34,5 +34,49 @@ const createPost = async (req: any, res: any) => {
     res.status(500).json({ error: 'Something went wrong when creating post' })
   }
 }
+const editPost = async (req: any, res: any) => {
+  const { content, privacy } = req.body
+  const { id } = req.params
+  if (!content) {
+    return res.status(422).json({ error: 'Write some content' })
+  }
 
-export { createPost }
+  try {
+    const editPost = {
+      content,
+      privacy,
+    }
+
+    await Post.updateOne({ _id: id }, editPost)
+    const post = await Post.findById(id).populate('user')
+    const postData = {
+      _id: post.id,
+      content: post.content,
+      privacy: post.privacy,
+      user: {
+        _id: post.user._id,
+        name: post.user.name,
+      },
+      createdAt: post.createdAt,
+    }
+
+    res
+      .status(200)
+      .json({ message: 'Post edited successfully', post: postData })
+  } catch (err) {
+    console.log(err)
+    res.status(500).json({ error: 'Something went wrong when editing post' })
+  }
+}
+const deletePost = async (req: any, res: any) => {
+  const { id } = req.params
+  try {
+    await Post.findByIdAndDelete(id)
+    res.json({ success: true })
+  } catch (err) {
+    console.log(err)
+    res.status(500).json({ error: 'Something went wrong when editing post' })
+  }
+}
+
+export { createPost, editPost, deletePost }
