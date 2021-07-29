9882f804d23dce8cd7e1e910c836351b29115792
ThomasTW935 <thomastw935@gmail.com>
2021-07-30 01:57:31 +0800

    Remove get By Post Id and Added get Post By User

--- controllers/Post/FetchPost.ts
+++ controllers/Post/FetchPost.ts
@@ -8,13 +8,15 @@ const fetchAllPost = async (req: any, res: any) => {
     res.status(500).json({ error: 'Database Error' })
   }
 }
-const fetchPostById = async (req: any, res: any) => {
+const fetchPostByUser = async (req: any, res: any) => {
   try {
-    const post = await Post.findById(req.params.postId).populate('user')
-    res.status(200).json({ post: post })
+    const posts = await Post.find({ user: req.params.userId })
+      .sort({ createdAt: -1 })
+      .populate('user')
+    res.status(200).json({ posts: posts })
   } catch {
     res.status(500).json({ error: 'Database Error' })
   }
 }
 
-export { fetchAllPost, fetchPostById }
+export { fetchAllPost, fetchPostByUser }
