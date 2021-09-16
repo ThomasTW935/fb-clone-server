91f958dd983885899bcc38e22be15d28512ea5cf
ThomasTW935 <thomastw935@gmail.com>
2021-09-16 09:37:40 +0800

    Fix reactions undefined names

--- src/controllers/Post/FetchPost.ts
+++ src/controllers/Post/FetchPost.ts
@@ -6,6 +6,7 @@ const fetchAllPost = async (req: any, res: any) => {
       .sort({ createdAt: -1 })
       .populate('user')
       .populate('reactions.user');
+
     res.status(200).json({ posts: posts });
   } catch {
     res.status(500).json({ error: 'Database Error' });
@@ -15,7 +16,8 @@ const fetchPostByUser = async (req: any, res: any) => {
   try {
     const posts = await Post.find({ user: req.params.userId })
       .sort({ createdAt: -1 })
-      .populate('user');
+      .populate('user')
+      .populate('reactions.user');
     res.status(200).json({ posts: posts });
   } catch {
     res.status(500).json({ error: 'Database Error' });
